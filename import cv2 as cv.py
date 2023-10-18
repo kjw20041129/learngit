736304be8ff02011e8b41d6c8ec0e1e7625a3fbd
Kang <2693289352@qq.com>
2023-10-18 20:22:15 +0800

    10.18

--- import cv2 as cv.py
+++ import cv2 as cv.py
@@ -8,14 +8,12 @@ cv.imshow('imgray', gray)
     # 2、二进制图像
 ret, binary = cv.threshold(gray, 100, 255, 0)
     # 阈值 二进制图像
-cv.imshow('binary', binary)
  
  
 # 提取轮廓
 
 contours, hierarchy = cv.findContours(binary, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     # 轮廓      层级                               轮廓检索模式(推荐此)  轮廓逼近方法
- 
     # 2、画出轮廓
 dst = cv.drawContours(img, contours, -1, (0, 255, 0), 3)
     #                           轮廓     第几个(默认-1：所有)   颜色       线条厚度
